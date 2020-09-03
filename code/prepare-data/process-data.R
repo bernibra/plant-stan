28c979f1c3e2808b55604bbc543bcac8791720bb
bernibra <bernat.bramon@gmail.com>
2020-09-03 17:23:07 +0200

    getting there, just figuring it out

--- code/prepare-data/process-data.R
+++ code/prepare-data/process-data.R
@@ -11,24 +11,8 @@ library(raster)
 ## Local library
 source("useful-tools.R")
 
-plot_cordinates <- function(x){
-  
-  # Load the data to use for our base map
-  data(wrld_simpl)
-  # Plot base map
-  extra <- 0
-  plot(wrld_simpl, 
-       xlim = c(min(x$long)-extra, max(x$long)+extra),
-       ylim = c(min(x$lat)-extra, max(x$lat)+extra),
-       axes = TRUE, 
-       col = "grey95")
-  
-  # Add original observations
-  points(x$long, x$lat, col = "olivedrab", pch = 20, cex = 0.75)
-  box()
-}
-
-prepare_data_SDM <- function(){
+## Prepare general data
+prepare.data <- function(){
   ## Load data
   dat.weighted <- read.csv("../../data/raw/distribution/grassland.abundance.csv", sep=";")
 
@@ -41,7 +25,8 @@ prepare_data_SDM <- function(){
   dat <- data.frame(Easting=x.weighted, Northing=y.weighted)
   coords <- cbind(Easting=x.weighted, Northing=y.weighted)
   swissgrid = "+proj=somerc +init=world:CH1903"
-  
+
+  # Define coordinates
   dat_SP <- SpatialPointsDataFrame(coords,
                                    data = dat,
                                    proj4string = CRS(swissgrid))
@@ -63,29 +48,28 @@ prepare_data_SDM <- function(){
   write.table(places.codes, file = "../../data/properties/codes/places_codes.csv", quote = F, row.names = F, col.names = c("place", "id", "richness", "easting", "northing"), sep=",")
   write.table(file="../../data/processed/distribution/distribution.csv", x=1*(dat.weighted>0), sep = ",",row.names = F, col.names = F, quote = F )
 
-  ## loop over speces to generate presence and absence data
-  for(i in 1:length(sp.weighted)){
-    idx <- dat.weighted[,i]>0
-    presence <- cbind(dat$long[idx], dat$lat[idx])
-    if(length(presence)>0){
-      write.table(presence, file = paste("../../data/processed/presence_lola/",as.character(i), ".csv", sep=""), quote = F, row.names = F, col.names = F, sep = ",")      
-    }
-  }
-  for(i in 1:length(sp.weighted)){
-    idx <- dat.weighted[,i]>0
-    presence <- cbind(dat$Easting[idx], dat$Northing[idx])
-    if(length(presence)>0){
-      write.table(presence, file = paste("../../data/processed/presence_eano/",as.character(i), ".csv", sep=""), quote = F, row.names = F, col.names = F, sep = ",")      
-    }
-  }
+  return(list(dat = dat.weighted, coord = dat_SP, places = places.codes, sp = sp.codes))
 }
 
-download_climatic <- function(res=0.5, meanlat, meanlong){
-  bioclim.data <- getData(name = "worldclim",
-                          var = "bio",
-                          res = res,
-                          lat=meanlat,
-                          lon=meanlong,
-                          path = "../../data/raw/climatic-data/")
+prepare.data.sdm <- function(){
+  
+  # ## loop over speces to generate presence and absence dat
+  # for(i in 1:length(sp.weighted)){
+  #   idx <- dat.weighted[,i]>0
+  #   presence <- cbind(dat$Easting[idx], dat$Northing[idx])
+  #   
+  #   if(length(presence)>0){
+  #     write.table(presence, file = paste("../../data/processed/presence_eano/",as.character(i), ".csv", sep=""), quote = F, row.names = F, col.names = F, sep = ",")
+  #   }
+  # }
+
+}
+
+prepare.data.jsdm <- function(){
+  
+}
+
+prepare.climatic <- function(){
+  
 }
 
