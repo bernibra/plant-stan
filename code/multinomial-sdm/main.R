6ee685034bb3d708d37743ce6c42ea0a653960e3
bernibra <bernat.bramon@gmail.com>
2020-10-25 18:50:50 +0100

    prepare for cluster the ordered categorical

--- code/multinomial-sdm/main.R
+++ code/multinomial-sdm/main.R
@@ -13,7 +13,7 @@ library(pROC)
 ####
 
 # This first one is just a ordered categorical regression, all "variables" are used as predictors in a linear form.
-ordered.categorical.stan <- function(d = NULL,idx=128, variables=c("bio5_", "bio6_","bio12_"), pseudoA=F, loglik=F, splitdata=F){
+ordered.categorical.stan <- function(d = NULL,idx=128, variables=c("bio5_", "bio6_","bio12_"), pseudoA=F, loglik=F, splitdata=F, show.plot=T, ofolder="../../results/models/"){
         
         # Load the data. This function will run maxent, but also process all the data for our stan model
         # The idea is that we can then compare the estimates from maxent and the estimates from our linear model
@@ -92,49 +92,53 @@ ordered.categorical.stan <- function(d = NULL,idx=128, variables=c("bio5_", "bio
         
         # Sample from the posterior
         post <- extract.samples(mfit_1.1)
-        
-        # Have a look at the estimates
-        plot(precis(mfit_1.1,depth=2))
-        
-        #Visualize plot
-        par(mfrow=c(3,3))
-        condi1 <- c(-1,-1,-1,0,0,0,1,1,1)
-        condi2 <- c(-1,0,1,-1,0,1,-1,0,1)
-        text <- c("bio12=-1 bio5=-1", "bio12=-1 bio5=0", "bio12=-1 bio5=1",
-                  "bio12=0 bio5=-1", "bio12=0 bio5=0", "bio12=0 bio5=1",
-                  "bio12=1 bio5=-1", "bio12=1 bio5=0", "bio12=1 bio5=1")
-        
-        for(k in 1:9){
-                dat_ <- dat
-                dat_$bio12_p_8110 <- dat_$bio12_p_8110*0+condi1[k]
-                dat_$bio5_tmaxw_8110 <- dat_$bio5_tmaxw_8110*0+condi2[k]
+
+        if(show.plot){
+                # Have a look at the estimates
+                plot(precis(mfit_1.1,depth=2))
                 
-                # Make predictions with test data
-                p_post <- link_1.1(post = post, dat=dat_)
-                p_mu <- lapply(1:length(p_post), function(x) apply( p_post[[x]] , 2 , mean ))
-                p_ci <- lapply(1:length(p_post), function(x) apply( p_post[[x]] , 2 , PI ))
+                #Visualize plot
+                par(mfrow=c(3,3))
+                condi1 <- c(-1,-1,-1,0,0,0,1,1,1)
+                condi2 <- c(-1,0,1,-1,0,1,-1,0,1)
+                text <- c("bio12=-1 bio5=-1", "bio12=-1 bio5=0", "bio12=-1 bio5=1",
+                          "bio12=0 bio5=-1", "bio12=0 bio5=0", "bio12=0 bio5=1",
+                          "bio12=1 bio5=-1", "bio12=1 bio5=0", "bio12=1 bio5=1")
                 
-                # Calculate AUC and stuff
-                plot( NULL , type="n" , xlab="tmin" , ylab="probability" ,
-                      xlim=c(min(dat$bio6_tminc_8110),max(dat$bio6_tminc_8110)) , ylim=c(0,1) , yaxp=c(0,1,2) )
-                
-                # for ( s in 1:50 ) {
-                #         for ( i in 1:3 ) lines( dat$bio6_tminc_8110 , p_post[[i]][s,] , col=col.alpha("black",0.1) )
-                # }
-                for ( i in 1:3 ) lines( dat$bio6_tminc_8110 , p_mu[[i]] , col=rangi2)
-                for ( i in 1:3 ) shade(   p_ci[[i]],dat$bio6_tminc_8110  , col=col.alpha(rangi2,0.2))
-                
-                title(text[k])
-        }
+                for(k in 1:9){
+                        dat_ <- dat
+                        dat_$bio12_p_8110 <- dat_$bio12_p_8110*0+condi1[k]
+                        dat_$bio5_tmaxw_8110 <- dat_$bio5_tmaxw_8110*0+condi2[k]
+                        
+                        # Make predictions with test data
+                        p_post <- link_1.1(post = post, dat=dat_)
+                        p_mu <- lapply(1:length(p_post), function(x) apply( p_post[[x]] , 2 , mean ))
+                        p_ci <- lapply(1:length(p_post), function(x) apply( p_post[[x]] , 2 , PI ))
+                        
+                        # Calculate AUC and stuff
+                        plot( NULL , type="n" , xlab="tmin" , ylab="probability" ,
+                              xlim=c(min(dat$bio6_tminc_8110),max(dat$bio6_tminc_8110)) , ylim=c(0,1) , yaxp=c(0,1,2) )
+                        
+                        # for ( s in 1:50 ) {
+                        #         for ( i in 1:3 ) lines( dat$bio6_tminc_8110 , p_post[[i]][s,] , col=col.alpha("black",0.1) )
+                        # }
+                        for ( i in 1:3 ) lines( dat$bio6_tminc_8110 , p_mu[[i]] , col=rangi2)
+                        for ( i in 1:3 ) shade(   p_ci[[i]],dat$bio6_tminc_8110  , col=col.alpha(rangi2,0.2))
+                        
+                        title(text[k])
+                }
+        }        
         
-        return(list(mfit = mfit_1.1, d=d))
+        out.object <- list(mfit = mfit_1.1, d=d)
+        saveRDS(out.object, file = paste(ofolder, "sdm-ordered-categorical.rds", sep=""))
+        return(out.object)
 }
 
 # This model is a bit more interesting. This is a zero-inflated ordered categorical distribution.
 # In this model, for every site, there is a probability p that a plant was there but was not observed.
 # To do so, we use a binomial distribution to decide whether or not the plant was there, and a second ordered categorical to decide whether or not we observed it and the abundance.
 # The aim of a zero-inflated model here is to avoid having to use pseudo-absences. 
-ordered.categorical.stan.zeroinflated <- function(d = NULL,idx=128, variables=c("bio5_", "bio6_","bio12_"), pseudoA=F, loglik=F, splitdata=F){
+ordered.categorical.stan.zeroinflated <- function(d = NULL,idx=128, variables=c("bio5_", "bio6_","bio12_"), pseudoA=F, loglik=F, splitdata=F, show.plot=T, ofolder="../../results/models/"){
         
         # Load the data. This function will run maxent, but also process all the data for our stan model
         # The idea is that we can then compare the estimates from maxent and the estimates from our linear model
@@ -215,46 +219,50 @@ ordered.categorical.stan.zeroinflated <- function(d = NULL,idx=128, variables=c(
         # Sample from the posterior
         post <- extract.samples(mfit_2.1)
         
-        # Have a look at the estimates
-        par(mfrow=c(1,1))
-        plot(precis(mfit_2.1,depth=2))
-        
-        #Visualize plot
-        par(mfrow=c(3,3))
-        condi1 <- c(-1,-1,-1,0,0,0,1,1,1)
-        condi2 <- c(-1,0,1,-1,0,1,-1,0,1)
-        text <- c("bio12=-1 bio6=-1", "bio12=-1 bio6=0", "bio12=-1 bio6=1",
-                  "bio12=0 bio6=-1", "bio12=0 bio6=0", "bio12=0 bio6=1",
-                  "bio12=1 bio6=-1", "bio12=1 bio6=0", "bio12=1 bio6=1")
-        
-        for(k in 1:9){
-                dat_ <- dat
-                dat_$bio12_p_8110 <- dat_$bio12_p_8110*0+condi1[k]
-                dat_$bio6_tminc_8110 <- dat_$bio6_tminc_8110*0+condi2[k]
+        if(show.plot){
+                # Have a look at the estimates
+                par(mfrow=c(1,1))
+                plot(precis(mfit_2.1,depth=2))
                 
-                # Make predictions with test data
-                p_post <- link_2.1(post = post, dat=dat_)
-                p_mu <- lapply(1:length(p_post), function(x) apply( p_post[[x]] , 2 , mean ))
-                p_ci <- lapply(1:length(p_post), function(x) apply( p_post[[x]] , 2 , PI ))
+                #Visualize plot
+                par(mfrow=c(3,3))
+                condi1 <- c(-1,-1,-1,0,0,0,1,1,1)
+                condi2 <- c(-1,0,1,-1,0,1,-1,0,1)
+                text <- c("bio12=-1 bio6=-1", "bio12=-1 bio6=0", "bio12=-1 bio6=1",
+                          "bio12=0 bio6=-1", "bio12=0 bio6=0", "bio12=0 bio6=1",
+                          "bio12=1 bio6=-1", "bio12=1 bio6=0", "bio12=1 bio6=1")
                 
-                # Calculate AUC and stuff
-                plot( NULL , type="n" , xlab="tmax" , ylab="probability" ,
-                      xlim=c(min(dat$bio5_tmaxw_8110),max(dat$bio5_tmaxw_8110)) , ylim=c(0,1) , yaxp=c(0,1,2) )
-                
-                # for ( s in 1:50 ) {
-                #         for ( i in 1:3 ) lines( dat$bio6_tminc_8110 , p_post[[i]][s,] , col=col.alpha("black",0.1) )
-                # }
-                for ( i in 1:3 ) lines( dat$bio5_tmaxw_8110 , p_mu[[i]] , col=rangi2)
-                for ( i in 1:3 ) shade(   p_ci[[i]],dat$bio5_tmaxw_8110  , col=col.alpha(rangi2,0.2))
-                
-                title(text[k])
+                for(k in 1:9){
+                        dat_ <- dat
+                        dat_$bio12_p_8110 <- dat_$bio12_p_8110*0+condi1[k]
+                        dat_$bio6_tminc_8110 <- dat_$bio6_tminc_8110*0+condi2[k]
+                        
+                        # Make predictions with test data
+                        p_post <- link_2.1(post = post, dat=dat_)
+                        p_mu <- lapply(1:length(p_post), function(x) apply( p_post[[x]] , 2 , mean ))
+                        p_ci <- lapply(1:length(p_post), function(x) apply( p_post[[x]] , 2 , PI ))
+                        
+                        # Calculate AUC and stuff
+                        plot( NULL , type="n" , xlab="tmax" , ylab="probability" ,
+                              xlim=c(min(dat$bio5_tmaxw_8110),max(dat$bio5_tmaxw_8110)) , ylim=c(0,1) , yaxp=c(0,1,2) )
+                        
+                        # for ( s in 1:50 ) {
+                        #         for ( i in 1:3 ) lines( dat$bio6_tminc_8110 , p_post[[i]][s,] , col=col.alpha("black",0.1) )
+                        # }
+                        for ( i in 1:3 ) lines( dat$bio5_tmaxw_8110 , p_mu[[i]] , col=rangi2)
+                        for ( i in 1:3 ) shade(   p_ci[[i]],dat$bio5_tmaxw_8110  , col=col.alpha(rangi2,0.2))
+                        
+                        title(text[k])
+                }
         }
         
-        return(list(mfit = mfit_2.1, d=d))
+        out.object <- list(mfit = mfit_2.1, d=d)
+        saveRDS(out.object, file = paste(ofolder, "sdm-ordered-categorical-zeroinflated.rds", sep=""))
+        return(out.object)
 }
 
 # This model might also be interesting. It is an ordered categorical regression, where all "variables" are used as predictors using gaussian RBF.
-ordered.categorical.stan.gaussian <- function(d = NULL,idx=128, variables=c("bio5_", "bio6_","bio12_"), pseudoA=F, loglik=F, splitdata=F){
+ordered.categorical.stan.gaussian <- function(d = NULL,idx=128, variables=c("bio5_", "bio6_","bio12_"), pseudoA=F, loglik=F, splitdata=F, show.plot=T, ofolder="../../results/models/"){
         
         # Load the data. This function will run maxent, but also process all the data for our stan model
         # The idea is that we can then compare the estimates from maxent and the estimates from our linear model
@@ -339,42 +347,46 @@ ordered.categorical.stan.gaussian <- function(d = NULL,idx=128, variables=c("bio
         # Sample from the posterior
         post <- extract.samples(mfit_3.1)
         
-        # Have a look at the estimates
-        par(mfrow=c(1,1))
-        plot(precis(mfit_3.1,depth=2))
-        
-        #Visualize plot
-        par(mfrow=c(3,3))
-        condi1 <- c(-1,-1,-1,0,0,0,1,1,1)
-        condi2 <- c(-1,0,1,-1,0,1,-1,0,1)
-        text <- c("bio12=-1 bio6=-1", "bio12=-1 bio6=0", "bio12=-1 bio6=1",
-                  "bio12=0 bio6=-1", "bio12=0 bio6=0", "bio12=0 bio6=1",
-                  "bio12=1 bio6=-1", "bio12=1 bio6=0", "bio12=1 bio6=1")
-        
-        for(k in 1:9){
-                dat_ <- dat
-                dat_$bio12_p_8110 <- dat_$bio12_p_8110*0+condi1[k]
-                dat_$bio6_tminc_8110 <- dat_$bio6_tminc_8110*0+condi2[k]
+        if(show.plot){
+                # Have a look at the estimates
+                par(mfrow=c(1,1))
+                plot(precis(mfit_3.1,depth=2))
                 
-                # Make predictions with test data
-                p_post <- link_3.1(post = post, dat=dat_)
-                p_mu <- lapply(1:length(p_post), function(x) apply( p_post[[x]] , 2 , mean ))
-                p_ci <- lapply(1:length(p_post), function(x) apply( p_post[[x]] , 2 , PI ))
+                #Visualize plot
+                par(mfrow=c(3,3))
+                condi1 <- c(-1,-1,-1,0,0,0,1,1,1)
+                condi2 <- c(-1,0,1,-1,0,1,-1,0,1)
+                text <- c("bio12=-1 bio6=-1", "bio12=-1 bio6=0", "bio12=-1 bio6=1",
+                          "bio12=0 bio6=-1", "bio12=0 bio6=0", "bio12=0 bio6=1",
+                          "bio12=1 bio6=-1", "bio12=1 bio6=0", "bio12=1 bio6=1")
                 
-                # Calculate AUC and stuff
-                plot( NULL , type="n" , xlab="tmax" , ylab="probability" ,
-                      xlim=c(min(dat$bio5_tmaxw_8110),max(dat$bio5_tmaxw_8110)) , ylim=c(0,1) , yaxp=c(0,1,2) )
-                
-                # for ( s in 1:50 ) {
-                #         for ( i in 1:3 ) lines( dat$bio6_tminc_8110 , p_post[[i]][s,] , col=col.alpha("black",0.1) )
-                # }
-                for ( i in 1:3 ) lines( dat$bio5_tmaxw_8110 , p_mu[[i]] , col=rangi2)
-                for ( i in 1:3 ) shade(   p_ci[[i]],dat$bio5_tmaxw_8110  , col=col.alpha(rangi2,0.2))
-                
-                title(text[k])
+                for(k in 1:9){
+                        dat_ <- dat
+                        dat_$bio12_p_8110 <- dat_$bio12_p_8110*0+condi1[k]
+                        dat_$bio6_tminc_8110 <- dat_$bio6_tminc_8110*0+condi2[k]
+                        
+                        # Make predictions with test data
+                        p_post <- link_3.1(post = post, dat=dat_)
+                        p_mu <- lapply(1:length(p_post), function(x) apply( p_post[[x]] , 2 , mean ))
+                        p_ci <- lapply(1:length(p_post), function(x) apply( p_post[[x]] , 2 , PI ))
+                        
+                        # Calculate AUC and stuff
+                        plot( NULL , type="n" , xlab="tmax" , ylab="probability" ,
+                              xlim=c(min(dat$bio5_tmaxw_8110),max(dat$bio5_tmaxw_8110)) , ylim=c(0,1) , yaxp=c(0,1,2) )
+                        
+                        # for ( s in 1:50 ) {
+                        #         for ( i in 1:3 ) lines( dat$bio6_tminc_8110 , p_post[[i]][s,] , col=col.alpha("black",0.1) )
+                        # }
+                        for ( i in 1:3 ) lines( dat$bio5_tmaxw_8110 , p_mu[[i]] , col=rangi2)
+                        for ( i in 1:3 ) shade(   p_ci[[i]],dat$bio5_tmaxw_8110  , col=col.alpha(rangi2,0.2))
+                        
+                        title(text[k])
+                }
         }
         
-        return(list(mfit = mfit_3.1, d=d))
+        out.object <- list(mfit = mfit_3.1, d=d)
+        saveRDS(out.object, file = paste(ofolder, "sdm-ordered-categorical-gaussian.rds", sep=""))
+        return(out.object)
 }
 
 
