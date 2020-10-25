547a5a1c6b94e2e4b54193167c0ace2404373187
bernibra <bernat.bramon@gmail.com>
2020-10-25 18:35:57 +0100

    Finished binnary sdms

--- code/binary-sdm/models.R
+++ code/binary-sdm/models.R
@@ -206,7 +206,7 @@ model{
 
   //priors
   alpha1 ~ normal(0,1.3);
-  alpha2 ~ normal(-1,0.3);
+  alpha2 ~ normal(0,1.3);
   beta ~ normal(0,0.5);
   
   for (i in 1:N) {
@@ -219,9 +219,9 @@ model{
      p2[i] = inv_logit(p2[i]);
      
      if (obs[i] == 0)
-        target += log_sum_exp(bernoulli_lpmf(0 | p1[i]), 
-                              bernoulli_lpmf(1 | p1[i])
-                               + bernoulli_lpmf(0 | p2[i]));
+        target += log_sum_exp(bernoulli_lpmf(0 | p2[i]), 
+                              bernoulli_lpmf(1 | p2[i])
+                               + bernoulli_lpmf(0 | p1[i]));
      else
         target +=  bernoulli_lpmf(1 | p1[i]) + bernoulli_lpmf(1 | p2[i]);
   }
@@ -230,7 +230,6 @@ model{
 
 # Model 4.0 is a zero-inflated logistic regression with quadratic terms
 # where I do not generate log-odds values so I can't calculate WAIC directly
-
 model4.0 <- "
 data{
   int N;
@@ -251,7 +250,7 @@ model{
 
   //priors
   alpha1 ~ normal(0,1.3);
-  alpha2 ~ normal(-1,0.3);
+  alpha2 ~ normal(0,1.3);
   beta ~ normal(0,0.5);
   beta2 ~ normal(0,0.25);
   
@@ -265,9 +264,9 @@ model{
      p2[i] = inv_logit(p2[i]);
      
      if (obs[i] == 0)
-        target += log_sum_exp(bernoulli_lpmf(0 | p1[i]), 
-                              bernoulli_lpmf(1 | p1[i])
-                               + bernoulli_lpmf(0 | p2[i]));
+        target += log_sum_exp(bernoulli_lpmf(0 | p2[i]), 
+                              bernoulli_lpmf(1 | p2[i])
+                               + bernoulli_lpmf(0 | p1[i]));
      else
         target +=  bernoulli_lpmf(1 | p1[i]) + bernoulli_lpmf(1 | p2[i]);
   }
@@ -330,6 +329,63 @@ generated quantities{
       }
       p[i] = inv_logit(p[i]);
       log_lik[i] = binomial_lpmf(obs[i] | 1, p[i]);
+    }
+}
+"
+
+# Model 6.0 is a zeroinflated logistic regression with a non-linear form (radial spline) for the different predictors
+# where I do not generate log-odds values so I can't calculate WAIC directly
+model6.0 <- "
+functions{
+  real radial(real x, real mu, real epsilon, real beta) {
+    real K;
+    K = x - mu;
+    K = K/epsilon;
+    K = pow(K, 2);
+    K = exp(-0.5*K);
+    K = beta * K;
+    return K;
+  }
+}
+data{
+  int N;
+  int K;
+  int obs[N];
+  real bio[N,K];
+}
+parameters{
+  real gamma;
+  real alpha;
+  vector[K] mu_bar;
+  vector[K] beta;
+  real<lower=0> epsilon[K];
+}
+model{
+  vector[N] p1;
+  vector[N] p2;
+
+  //priors
+  alpha ~ normal(0,1);
+  gamma ~ normal(0,1);
+  mu_bar ~ normal(0,1);
+  beta ~ normal(0,1);
+  epsilon ~ exponential( 1 );
+  
+  for (i in 1:N) {
+     p1[i] = alpha;
+     p2[i] = gamma;
+     for (j in 1:K){
+          p1[i] = p1[i] + radial(bio[i, j], mu_bar[j], epsilon[j], beta[j]);
+     }
+     p1[i] = inv_logit(p1[i]);
+     p2[i] = inv_logit(p2[i]);
+     
+     if (obs[i] == 0)
+        target += log_sum_exp(bernoulli_lpmf(0 | p2[i]), 
+                              bernoulli_lpmf(1 | p2[i])
+                               + bernoulli_lpmf(0 | p1[i]));
+     else
+        target +=  bernoulli_lpmf(1 | p1[i]) + bernoulli_lpmf(1 | p2[i]);
     }
 }
 "
