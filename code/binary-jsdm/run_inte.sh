00e56c8978c012d44c81ba3b4ca387ed422cb6b7
bernibra <bernat.bramon@gmail.com>
2020-10-26 19:16:27 +0100

    Added number of cores

--- code/binary-jsdm/run_inte.sh
+++ code/binary-jsdm/run_inte.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-bsub -R "rusage[mem=10048]" -W 8:00 "R --vanilla --slave < model-simulated-data.R"
+bsub -R "rusage[mem=20048]" -W 8:00 -n 3 "R --vanilla --slave < model-simulated-data.R"
 
