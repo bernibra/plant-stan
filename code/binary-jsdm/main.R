d81b592482db80112001dcbe2833db40aa6f21c3
bernibra <bernat.bramon@gmail.com>
2020-10-29 12:18:21 +0100

    Fixing non-centered linear jsdm

--- code/binary-jsdm/main.R
+++ code/binary-jsdm/main.R
@@ -1,4 +1,4 @@
-# source("./prepare-data.R")
+source("./prepare-data.R")
 source("./models.R")
 library(rethinking)
 library(rstan)
@@ -11,6 +11,14 @@ library(rstan)
 # This first one is just a logistic regression, all "variables" are used as predictors in a linear form.
 binomial.stan <- function(d = NULL, variables=c("bio5_", "bio6_","bio12_"), recompile = T, loglik=T, show.plot=T, pca=F, ndim=2, simulated=F, ofolder="../../results/models/"){
                 
+        # File name extension
+        extension <- ""
+        
+        # If we are dealing with simulated data
+        if(simulated){
+                extension <- "-simulated"
+        }
+        
         # Load the data
         if(is.null(d)){
                 if(recompile){
@@ -52,27 +60,44 @@ binomial.stan <- function(d = NULL, variables=c("bio5_", "bio6_","bio12_"), reco
                         bio=bio,
                         id=id)
         
-        # Set starting values for the parameters
-        start_1.1 <- list(
-                alpha = rep(0, dat_1.1$L),
-                zalpha = 0,
-                sigma_a = 0.1,
-                beta = matrix(0,dat_1.1$L,dat_1.1$K),
-                zbeta = rep(0, dat_1.1$K),
-                sigma_b = rep(0.1, dat_1.1$K)
-        )
+
+        
+        if (non.centered){
+                # Set starting values for the parameters
+                start_1.1 <- list(
+                        zalpha = rep(0, dat_1.1$L),
+                        alpha_bar = 0,
+                        sigma_a = 0.1,
+                        zbeta = matrix(0,dat_1.1$K,dat_1.1$L),
+                        beta_bar = rep(0, dat_1.1$K),
+                        sigma_b = rep(0.1, dat_1.1$K)
+                )
+                
+                model_code=model1.2
+                
+        }else{
+                # Set starting values for the parameters
+                start_1.1 <- list(
+                        zalpha = rep(0, dat_1.1$L),
+                        alpha = 0,
+                        sigma_a = 0.1,
+                        beta = matrix(0,dat_1.1$L,dat_1.1$K),
+                        zbeta = rep(0, dat_1.1$K),
+                        sigma_b = rep(0.1, dat_1.1$K)
+                )
+                
+                # Should I calculate the likelihood?
+                if(loglik){
+                        model_code=model1.1
+                }else{
+                        model_code=model1.0     
+                }
+        }
         
         # Initialize data structure
         n_chains_1.1 <- 3
         init_1.1 <- list()
         for ( i in 1:n_chains_1.1 ) init_1.1[[i]] <- start_1.1
-        
-        # Should I calculate the likelihood?
-        if(loglik){
-                model_code=model1.1
-        }else{
-                model_code=model1.0     
-        }
         
         # Run stan model
         mfit_1.1 <- stan ( model_code=model_code ,
@@ -82,38 +107,7 @@ binomial.stan <- function(d = NULL, variables=c("bio5_", "bio6_","bio12_"), reco
                            warmup=1000, iter=4000,
                            init=init_1.1 , control = list(adapt_delta = 0.95))
         
-
-        # dat <- list(obs=obs, id=id, bio1=bio$PC1, bio2=bio$PC2)
-        # 
-        # # Use rethinking package to fit stan model
-        # m1 <- ulam(
-        #         alist(
-        #                 obs ~ dbinom( 1 , p ),
-        #                 logit(p) <- alpha[id] * sigma_alpha + alpha_bar + beta[id, 1] * bio1 + beta[id, 2] * bio2,
-        #                 transpars> matrix[id,2]:beta <- compose_noncentered( sigma_id , L_Rho_id , z_id ),
-        #                 matrix[2,id]:z_id ~ dnorm( 0 , 1),
-        #                 alpha[id] ~ dnorm(0,1),
-        #                 alpha_bar ~ dnorm(0,1),
-        #                 sigma_alpha ~ dexp(1),
-        #                 cholesky_factor_corr[2]:L_Rho_id ~ lkj_corr_cholesky( 2 ),
-        #                 vector[2]:sigma_id ~ dexp(1),
-        #                 gq> matrix[2,2]:Rho_id <<- multiply_lower_tri_self_transpose(L_Rho_id)
-        #         ) , data=dat , chains=3,  iter = 3000,  log_lik = T, cores = 3)
-        # 
-        # # Use rethinking package to fit stan model
-        # m2 <- ulam(
-        #         alist(
-        #                 obs ~ dbinom( 1 , p ),
-        #                 logit(p) <- a[id] + b[id] * bio1 + c[id] * bio2,
-        #                 c(a,b,c)[id] ~ multi_normal(c(za,zb,zc),Rho_id,sigma_a),
-        #                 za ~ normal(0,1.3),
-        #                 zb ~ normal(0,1.3),
-        #                 zc ~ normal(0,1.3),
-        #                 sigma_a ~ dexp(1),
-        #                 Rho_id ~ lkj_corr(2)
-        #         ) , data=dat , chains=3,  iter = 3000,  log_lik = T, cores = 3)
-        # 
-        saveRDS(mfit_1.1, file = paste(ofolder, "binomial-jsdm.rds", sep=""))
+        saveRDS(mfit_1.1, file = paste(ofolder, "binomial-jsdm",extension,".rds", sep=""))
         return(mfit_1.1)
 }
 
--- code/binary-jsdm/models.R
+++ code/binary-jsdm/models.R
@@ -112,80 +112,56 @@ data{
 }
 parameters{
     vector[L] zalpha;
-    real alpha;
+    real alpha_bar;
     real<lower=0> sigma_a;
-    matrix[2,10] zbeta;
+    matrix[K,L] zbeta;
+    vector[K] beta_bar;
     vector<lower=0>[K] sigma_b;
     cholesky_factor_corr[K] L_Rho_b;
 }
 transformed parameters{
     matrix[L,K] beta;
+    vector[L] alpha;
     beta = (diag_pre_multiply(sigma_b, L_Rho_b) * zbeta)';
+    for (i in 1:L){
+        alpha[i] = zalpha[i] * sigma_a + alpha_bar;
+        for (j in 1:K){
+          beta[i, j] = beta[i, j] + beta_bar[j];
+        }
+    }
 }
 model{
     vector[N] p;
     sigma_b ~ exponential( 1 );
-    L_Rho_b ~ lkj_corr_cholesky( 2 );
     sigma_a ~ exponential( 1 );
+    L_Rho_b ~ lkj_corr_cholesky( 2 );
     zalpha ~ normal( 0 , 1 );
-    alpha ~ normal( 0 , 1.3 );
-    to_vector( z_id ) ~ normal( 0 , 1 );
-    for ( i in 1:9120 ) {
-        p[i] = zalpha[id[i]] * sigma_a + alpha + beta[id[i], 1] * bio1[i] + beta[id[i], 2] * bio2[i];
-        p[i] = inv_logit(p[i]);
+    to_vector( zbeta ) ~ normal( 0 , 1 );
+    alpha_bar ~ normal( 0 , 1.3 );
+    beta_bar ~ normal( 0 , 1.3 );
+    
+    for (i in 1:N) {
+       p[i] = alpha[id[i]];
+       for (j in 1:K){
+          p[i] = p[i] + beta[id[i], j] * bio[i, j];
+       }
+       p[i] = inv_logit(p[i]);
     }
     obs ~ binomial( 1 , p );
 }
 generated quantities{
-    matrix[2,2] Rho_id;
-    Rho_id = multiply_lower_tri_self_transpose(L_Rho_id);
-}
-"
-
-
-bla <- "
-data{
-  int obs[9120];
-  vector[9120] bio2;
-  vector[9120] bio1;
-  int id[9120];
-}
-parameters{
-  matrix[2,10] z_id;
-  vector[10] alpha;
-  real alpha_bar;
-  real<lower=0> sigma_alpha;
-  cholesky_factor_corr[2] L_Rho_id;
-  vector<lower=0>[2] sigma_id;
-}
-transformed parameters{
-  matrix[10,2] beta;
-  beta = (diag_pre_multiply(sigma_id, L_Rho_id) * z_id)';
-}
-model{
-    vector[9120] p;
-    sigma_id ~ exponential( 1 );
-    L_Rho_id ~ lkj_corr_cholesky( 2 );
-    sigma_alpha ~ exponential( 1 );
-    alpha_bar ~ normal( 0 , 1 );
-    alpha ~ normal( 0 , 1 );
-    to_vector( z_id ) ~ normal( 0 , 1 );
-    for ( i in 1:9120 ) {
-        p[i] = alpha[id[i]] * sigma_alpha + alpha_bar + beta[id[i], 1] * bio1[i] + beta[id[i], 2] * bio2[i];
-        p[i] = inv_logit(p[i]);
+    vector[N] log_lik;
+    vector[N] p;
+    matrix[2,2] Rho_b;
+    Rho_b = multiply_lower_tri_self_transpose(L_Rho_b);
+    
+    for (i in 1:N) {
+       p[i] = alpha[id[i]];
+       for (j in 1:K){
+          p[i] = p[i] + beta[id[i], j] * bio[i, j];
+       }
+       p[i] = inv_logit(p[i]);
+       log_lik[i] = binomial_lpmf( obs[i] | 1 , p[i] );
     }
-    obs ~ binomial( 1 , p );
 }
-generated quantities{
-    vector[9120] log_lik;
-    vector[9120] p;
-    matrix[2,2] Rho_id;
-    Rho_id = multiply_lower_tri_self_transpose(L_Rho_id);
-    for ( i in 1:9120 ) {
-        p[i] = alpha[id[i]] * sigma_alpha + alpha_bar + beta[id[i], 1] * bio1[i] + beta[id[i], 2] * bio2[i];
-        p[i] = inv_logit(p[i]);
-    }
-    for ( i in 1:9120 ) log_lik[i] = binomial_lpmf( obs[i] | 1 , p[i] );
-}
-
 "
