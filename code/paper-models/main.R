82b3a501c0691de22d19c1537f2a42248e4589a9
bernibra <bernat.bramon@gmail.com>
2021-02-19 17:40:11 +0100

    Adding categorical models that work

--- code/paper-models/main.R
+++ code/paper-models/main.R
@@ -375,7 +375,7 @@ categorical.1d <- function(d = NULL, recompile = T, simulated=T, min.occurrence=
         
         # If we are dealing with simulated data
         if(simulated){
-                extension <- "1d-simulated"
+                extension <- "simulated-categorical"
         }
         extension <- paste(extension, as.character(gp_type), sep="")
         
@@ -447,6 +447,7 @@ categorical.1d <- function(d = NULL, recompile = T, simulated=T, min.occurrence=
                         M=length(unique(as.vector(obs)))-1,
                         Y=obs,
                         X1=X1,
+                        minp=0.0000000001,
                         Dmat_b=Dis_b,
                         Dmat_g=Dis_g
         )
@@ -472,7 +473,7 @@ categorical.1d <- function(d = NULL, recompile = T, simulated=T, min.occurrence=
         model_code=categorical.model.1d
         
         # Initialize data structure
-        n_chains_5.1 <- 3
+        n_chains_5.1 <- 15
         init_5.1 <- list()
         for ( i in 1:n_chains_5.1 ) init_5.1[[i]] <- start_5.1
         
@@ -481,11 +482,141 @@ categorical.1d <- function(d = NULL, recompile = T, simulated=T, min.occurrence=
                            data=dat_5.1 ,
                            chains=n_chains_5.1 ,
                            cores= n_chains_5.1 ,
-                           warmup=1000, iter=2000,
+                           warmup=1000, iter=1200,
                            init=init_5.1 , control = list(adapt_delta = 0.95, max_treedepth = 15))
         
         
         saveRDS(mfit_5.1, file = paste(ofolder, extension2, "categorical-model-1d", extension,".rds", sep=""))
+        return(mfit_5.1)
+}
+
+categorical.skew.1d <- function(d = NULL, recompile = T, simulated=T, min.occurrence=10, ofolder="../../results/models/"){
+        # Fixing some of the options
+        variables=c("bio5_", "bio6_","bio12_", "gdd5_", "bio1_","bio15_","bio17_", "bio8_", "TabsY_")
+        gp_type <- 2
+        ndim <- 2
+        pca <- T
+        
+        # File name extension
+        extension <- ""
+        
+        # If we are dealing with simulated data
+        if(simulated){
+                extension <- "simulated-categorical"
+        }
+        extension <- paste(extension, as.character(gp_type), sep="")
+        
+        if(min.occurrence==10){
+                extension2 <- ""
+        }else{
+                extension2 <- "min30-"
+        }
+        
+        
+        # Load the data
+        if(recompile){
+                d <- species_distribution.data(variables=variables, pca=pca, ndim = ndim, simulated=simulated, simulated.type="categorical", min.occurrence=min.occurrence)
+                # rename variables
+                if(simulated){
+                        variables <- c("S1", "S2")
+                }else{
+                        if(pca){
+                                variables <- paste("PC", 1:ndim, sep="")
+                        }
+                }
+                filename <- paste("../../data/processed/jsdm/", extension, paste(variables, collapse = ""), extension2, "data.rds", sep = "")
+                
+                if (file.exists(filename)){
+                        question <- askYesNo("Do you want to overwrite the file?", default = F, 
+                                             prompts = getOption("askYesNo", gettext(c("Yes", "No", "Cancel"))))
+                        if(is.na(question)){question <- F}
+                        if(question){
+                                saveRDS(d, file=filename)
+                        }else{
+                                stop("you should add 'recompile=T'")
+                        }                                
+                }else{
+                        saveRDS(d, file=filename)
+                }
+        }else{
+                # rename variables
+                if(simulated){
+                        variables <- c("S1", "S2")
+                }else{
+                        if(pca){
+                                variables <- paste("PC", 1:ndim, sep="")
+                        }
+                }
+                if(is.null(d)){
+                        d <- readRDS(file = paste("../../data/processed/jsdm/", extension, paste(variables, collapse = ""), extension2, "data.rds", sep = ""))
+                }
+        }
+        
+        # Prepare training data for stan model
+        Dis_b <- d$corr
+        Dis_g <- d$corr2
+        N <- sum(d$dataset$id==1)
+        L <- length(unique(d$dataset$id))
+        if(simulated){
+                obs <- d$dataset$obs
+        }else{
+                obs <- d$dataset$abundance
+        }
+        obs <- as.numeric(as.character(obs))
+        obs <- matrix(obs, N, L)
+        dat <- d$dataset
+        id <- dat$id
+        bio <- dat[,(ncol(dat)-length(variables)+1):ncol(dat)]
+        X1 <- matrix(bio[,1], N, L)[,1]
+        
+        dat_5.1 <- list(N=N,
+                        L=L,
+                        M=length(unique(as.vector(obs)))-1,
+                        Y=obs,
+                        X1=X1,
+                        minp=0.0000000001,
+                        Dmat_b=Dis_b,
+                        Dmat_g=Dis_g
+        )
+        
+        # Set starting values for the parameters
+        start_5.1 <- list(
+                phi = (1:dat_5.1$M)/dat_5.1$M-0.5,
+                zlambda = rep(0, dat_5.1$L),
+                zalpha = rep(0, dat_5.1$L),
+                zbeta = rep(0, dat_5.1$L),
+                zgamma = rep(0, dat_5.1$L),
+                lambda_bar = 0,
+                alpha_bar = 0,
+                beta_bar = 0,
+                gamma_bar = 0,
+                sigma_l = 0.1,
+                sigma_a = 0.1,
+                sigma_b = 0.1,
+                etasq_b = 0.1,
+                rhosq_b = 0.1,
+                sigma_g = 0.1,
+                etasq_g = 0.1,
+                rhosq_g = 0.1
+        )
+        
+        model_code=categorical.model.skew.1d
+        
+        # Initialize data structure
+        n_chains_5.1 <- 15
+        init_5.1 <- list()
+        for ( i in 1:n_chains_5.1 ) init_5.1[[i]] <- start_5.1
+        
+        # Run stan model
+        mfit_5.1 <- stan ( model_code=model_code ,
+                           data=dat_5.1 ,
+                           chains=n_chains_5.1 ,
+                           cores= n_chains_5.1 ,
+                           warmup=1000, iter=1200,
+                           init=init_5.1 , control = list(adapt_delta = 0.95, max_treedepth = 15))
+        
+        
+        saveRDS(mfit_5.1, file = paste(ofolder, extension2, "categorical-model-skew-1d", extension,".rds", sep=""))
         return(mfit_5.1)
 }
 
@@ -616,7 +747,9 @@ skew.1d <- function(d = NULL, recompile = T, simulated=T, min.occurrence=10, ofo
 # skew.1d(d=d, simulated=T, recompile = F, min.occurrence = 10, ofolder="/cluster/scratch/bemora/plant-stan/")
 # baseline.1d(d=d, simulated=T, recompile = F, ofolder="/cluster/scratch/bemora/plant-stan/")
 
-categorical.1d(d=NULL, simulated=F, recompile=T, min.occurrence = 30)
+d <- readRDS(file = "../../data/processed/jsdm/1d-simulated2S1S2categorical-data.rds")
+categorical.1d(d=NULL, simulated=T, recompile=F, min.occurrence = 10)
+# categorical.skew.1d(d=NULL, simulated=T, recompile=F, min.occurrence = 10)
 
 # # 
 # expose_stan_functions(mod)
--- code/paper-models/models.R
+++ code/paper-models/models.R
@@ -421,6 +421,7 @@ data{
     int M;
     int Y[N,L];
     vector[N] X1;
+    real minp;
     matrix[L,L] Dmat_b;
     matrix[L,L] Dmat_g;
 }
@@ -484,13 +485,146 @@ model{
         for (j in 1:N){
            p = exp(- alpha[i] - gamma[i] * pow(X1[j] - beta[i],2));
 
-           prob[1] = 1 - phi[1]*p - 0.0000000001*(M+1);
+           prob[1] = 1 - phi[1]*p - minp*(M+1);
            
            for (k in 2:M){
-               prob[k]  =  phi[k-1]*p - phi[k]*p + 0.0000000001;
+               prob[k]  =  phi[k-1]*p - phi[k]*p + minp;
            }
            
-           prob[M+1]  = phi[M]*p + 0.0000000001;
+           prob[M+1]  = phi[M]*p + minp;
+           //print(prob);
+
+           Y[j,i] ~ categorical(prob);
+        }
+    }
+}
+"
+
+# Categorical 1d
+categorical.model.skew.1d <- "
+functions{
+    matrix cov_GPL2(matrix x, real a, real b, real delta) {
+        int N = dims(x)[1];
+        matrix[N, N] K;
+
+        for (i in 1:(N-1)) {
+          K[i, i] = a + delta;
+          for (j in (i + 1):N) {
+            K[i, j] = a * exp(- b * square(x[i,j]) );
+            K[j, i] = K[i, j];
+          }
+        }
+        K[N, N] = a + delta;
+        return K;
+    }
+    int sgn(real x) {
+        return x < 0 ? -1 : x > 0;
+    }
+    real findmax(real delta, real beta, real gamma, real lambda) {
+        real maxy;
+        real muz;
+        maxy = 0.5 * ( 4.0 - pi() ) * pow((delta * sqrt( 2.0 / pi() )),3) / pow(1.0 - 2.0 * pow(delta,2) / pi() ,1.5);
+        muz = sqrt( 2.0 / pi() ) * delta;
+        maxy = beta + (1.0 / sqrt( 2.0 * gamma)) * (muz - maxy * sqrt( 1.0 - muz * muz ) * 0.5 - 0.5 * sgn(lambda) * exp(- 2.0 * pi() / fabs(lambda) ));
+        maxy = exp(- gamma * pow(maxy - beta,2)) * (1.0 + erf((lambda * ( maxy - beta )) * sqrt( gamma ) ));
+        return maxy+0.0001;
+    }
+}
+data{
+    int N;
+    int L;
+    int M;
+    int Y[N,L];
+    vector[N] X1;
+    real minp;
+    matrix[L,L] Dmat_b;
+    matrix[L,L] Dmat_g;
+}
+parameters{
+    ordered[M] zphi;
+    vector[L] zalpha;
+    vector[L] zbeta;
+    vector[L] zgamma;
+    vector<lower=-10, upper=10>[L] zlambda;
+    real alpha_bar;
+    real beta_bar;
+    real gamma_bar;
+    real lambda_bar;
+    real<lower=0> sigma_l;
+    real<lower=0> sigma_a;
+    real<lower=0> sigma_b;
+    real<lower=0> etasq_b;
+    real<lower=0> rhosq_b;
+    real<lower=0> sigma_g;
+    real<lower=0> etasq_g;
+    real<lower=0> rhosq_g;
+}
+transformed parameters{
+    vector[L] lambda;
+    vector[L] alpha;
+    vector[L] beta;
+    vector[L] gamma;
+    matrix[L, L] L_SIGMA_b;
+    matrix[L, L] L_SIGMA_g;
+    vector<lower=0>[M] phi;
+    real delta;
+
+    lambda = zlambda * sigma_l + lambda_bar;
+
+    alpha = exp(zalpha * sigma_a + alpha_bar);
+
+    L_SIGMA_b = cholesky_decompose(cov_GPL2(Dmat_b, etasq_b, rhosq_b, sigma_b));
+    beta = L_SIGMA_b * zbeta + beta_bar;
+
+    L_SIGMA_g = cholesky_decompose(cov_GPL2(Dmat_g, etasq_g, rhosq_g, sigma_g));
+    gamma = L_SIGMA_g * zgamma + gamma_bar;
+    gamma = exp(gamma);
+    
+    for(i in 1:M){
+        phi[i] = exp(-exp(zphi[i]));
+    }
+    
+    for (i in 1:L){
+        delta = lambda[i] / ( sqrt( 1.0 + (pow(lambda[i],2)) ));
+        gamma[i] = gamma[i] * (1.0 - (2 * pow(delta,2))/pi());
+        beta[i] = beta[i] - sqrt( 1.0 / (2 * gamma[i]) ) * (delta * sqrt(2/pi()));
+        alpha[i] = log(findmax(delta, beta[i], gamma[i], lambda[i])) + alpha[i];
+    }
+
+}
+model{
+    real p;
+    vector[M+1] prob;
+
+    sigma_a ~ exponential( 1 );
+    sigma_b ~ exponential( 1 );
+    sigma_g ~ exponential( 1 );
+    sigma_l ~ exponential( 1 );
+    etasq_b ~ exponential( 1 );
+    etasq_g ~ exponential( 1 );
+    rhosq_b ~ exponential( 0.5 );
+    rhosq_g ~ exponential( 0.5 );
+    alpha_bar ~ normal( 0 , 1.3 );
+    beta_bar ~ std_normal();
+    gamma_bar ~ std_normal();
+    lambda_bar ~ std_normal();
+    zalpha ~ std_normal();
+    zgamma ~ std_normal();
+    zbeta ~ std_normal();
+    zlambda ~ std_normal();
+    zphi ~ std_normal();
+    
+    for ( i in 1:L ){
+        for (j in 1:N){
+           p = exp(- alpha[i] - gamma[i] * pow(X1[j] - beta[i],2)) * (1 + erf((lambda[i] * (X1[j] - beta[i])) * sqrt(gamma[i]) ));
+
+           prob[1] = 1 - phi[1]*p - minp*(M+1);
+           
+           for (k in 2:M){
+               prob[k]  =  phi[k-1]*p - phi[k]*p + minp;
+           }
+           
+           prob[M+1]  = phi[M]*p + minp;
            //print(prob);
 
            Y[j,i] ~ categorical(prob);
