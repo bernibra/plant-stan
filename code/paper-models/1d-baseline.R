339709dbdb86f30196b3661e8531ef703ce378e9
bernibra <bernat.bramon@gmail.com>
2021-03-02 13:28:39 +0100

    Adding generalized error distributino

--- code/paper-models/1d-baseline.R
+++ code/paper-models/1d-baseline.R
@@ -128,6 +128,127 @@ baseline.1d <- function(d = NULL, recompile = T, simulated=T, min.occurrence=10,
   return(mfit_5.1)
 }
 
+generror.1d <- function(d = NULL, recompile = T, simulated=T, min.occurrence=10, ofolder="../../results/models/"){
+  # Fixing some of the options
+  variables=c("bio5_", "bio6_","bio12_", "gdd5_", "bio1_","bio15_","bio17_", "bio8_", "TabsY_")
+  ndim <- 2
+  pca <- T
+  
+  # If we are dealing with simulated data
+  if(simulated){
+    extension <- "1d-generror-simulated"
+  }else{
+    extension <- "1d-generror"
+  }
+  
+  if(min.occurrence==10){
+    extension2 <- "-"
+  }else{
+    extension2 <- paste("min",min.occurrence,"-",sep="")
+  }
+  
+  # Load the data
+  if(recompile){
+    d <- species_distribution.data(variables=variables, pca=pca, ndim = ndim, simulated=simulated, simulated.type="gauss.gauss", min.occurrence=min.occurrence)
+    # rename variables
+    if(simulated){
+      variables <- c("S1", "S2")
+    }else{
+      if(pca){
+        variables <- paste("PC", 1:ndim, sep="")
+      }
+    }
+    filename <- paste("../../data/processed/jsdm/", extension, "-",paste(variables, collapse = ""), extension2, "data.rds", sep = "")
+    
+    if (file.exists(filename)){
+      question <- askYesNo("Do you want to overwrite the file?", default = F, 
+                           prompts = getOption("askYesNo", gettext(c("Yes", "No", "Cancel"))))
+      if(is.na(question)){question <- F}
+      if(question){
+        saveRDS(d, file=filename)
+      }else{
+        stop("you should add 'recompile=T'")
+      }                                
+    }else{
+      saveRDS(d, file=filename)
+    }
+  }else{
+    # rename variables
+    if(simulated){
+      variables <- c("S1", "S2")
+    }else{
+      if(pca){
+        variables <- paste("PC", 1:ndim, sep="")
+      }
+    }
+    if(is.null(d)){
+      d <- readRDS(file = paste("../../data/processed/jsdm/", extension,"-", paste(variables, collapse = ""), extension2, "data.rds", sep = ""))
+    }
+  }
+  
+  # Prepare training data for stan model
+  Dis_b <- d$corr
+  Dis_g <- d$corr2
+  N <- sum(d$dataset$id==1)
+  L <- length(unique(d$dataset$id))
+  obs <- matrix(d$dataset$obs, N, L)
+  dat <- d$dataset
+  id <- dat$id
+  bio <- dat[,(ncol(dat)-length(variables)+1):ncol(dat)]
+  X1 <- matrix(bio[,1], N, L)[,1]
+  
+  dat_5.1 <- list(N=N,
+                  L=L,
+                  minp=1e-100,
+                  Y=t(obs),
+                  X1=X1,
+                  Dmat_b=Dis_b,
+                  Dmat_g=Dis_g
+  )
+  
+  # Set starting values for the parameters
+  start_5.1 <- list(
+    zalpha = rep(0, dat_5.1$L),
+    zbeta = rep(0, dat_5.1$L),
+    zgamma = rep(0, dat_5.1$L),
+    znu = rep(0, dat_5.1$L),
+    alpha_bar = 0,
+    nu_bar = 0,
+    beta_bar = 0,
+    gamma_bar = 0,
+    sigma_a = 0.1,
+    sigma_n = 0.1,
+    etasq_a = 0.1,
+    rhosq_a = 0.1,
+    sigma_b = 0.1,
+    etasq_b = 0.1,
+    rhosq_b = 0.1,
+    sigma_g = 0.1,
+    etasq_g = 0.1,
+    rhosq_g = 0.1
+  )
+  
+  model_code=base.model.generror.1d
+  
+  # Initialize data structure
+  n_chains_5.1 <- 3
+  init_5.1 <- list()
+  for ( i in 1:n_chains_5.1 ) init_5.1[[i]] <- start_5.1
+  
+  # Run stan model
+  mfit_5.1 <- stan ( model_code=model_code ,
+                     data=dat_5.1 ,
+                     chains=n_chains_5.1 ,
+                     cores= n_chains_5.1 ,
+                     warmup=1000, iter=2000,
+                     init=init_5.1 , control = list(adapt_delta = 0.95, max_treedepth = 15))
+  
+  
+  saveRDS(mfit_5.1, file = paste(ofolder, extension2, "", extension,".rds", sep=""))
+  return(mfit_5.1)
+}
+
+
 skew.1d <- function(d = NULL, recompile = T, simulated=T, min.occurrence=10, ofolder="../../results/models/"){
   # Fixing some of the options
   variables=c("bio5_", "bio6_","bio12_", "gdd5_", "bio1_","bio15_","bio17_", "bio8_", "TabsY_")
@@ -246,13 +367,14 @@ skew.1d <- function(d = NULL, recompile = T, simulated=T, min.occurrence=10, ofo
   return(mfit_5.1)
 }
 
-min.occurrence <- 10
+min.occurrence <- 30
 if(min.occurrence==10){
   d <- readRDS(file = paste("../../data/processed/jsdm/1d-PC1PC2-data.rds", sep=""))
 }else{
   d <- readRDS(file = paste("../../data/processed/jsdm/1d-PC1PC2min",min.occurrence,"-data.rds", sep=""))
 }
-skew.1d(d=d, simulated=F, recompile = F, min.occurrence = min.occurrence, ofolder="/cluster/scratch/bemora/plant-stan/")
+generror.1d(d=d, simulated=F, recompile = F, min.occurrence = min.occurrence, ofolder="/cluster/scratch/bemora/plant-stan/")
+# skew.1d(d=d, simulated=F, recompile = F, min.occurrence = min.occurrence, ofolder="/cluster/scratch/bemora/plant-stan/")
 # baseline.1d(d=d, simulated=F, recompile = F, min.occurrence = min.occurrence, ofolder="/cluster/scratch/bemora/plant-stan/")
 
 
--- code/paper-models/models-binomial.R
+++ code/paper-models/models-binomial.R
@@ -97,7 +97,115 @@ generated quantities{
 }
 "
 
+# Binomial 1d
+base.model.generror.1d <- "
+functions{
+    matrix cov_GPL2(matrix x, real a, real b, real delta) {
+        int N = dims(x)[1];
+        matrix[N, N] K;
 
+        for (i in 1:(N-1)) {
+          K[i, i] = a + delta;
+          for (j in (i + 1):N) {
+            K[i, j] = a * exp(- b * square(x[i,j]) );
+            K[j, i] = K[i, j];
+          }
+        }
+        K[N, N] = a + delta;
+        return K;
+    }
+}
+data{
+    int N;
+    int L;
+    real minp;
+    // int K;
+    // int Y[K];
+    int Y[L, N];
+    row_vector[N] X1;
+    matrix[L,L] Dmat_b;
+    matrix[L,L] Dmat_g;
+}
+parameters{
+    vector[L] zalpha;
+    vector[L] zbeta;
+    vector[L] zgamma;
+    vector[L] znu;
+    real alpha_bar;
+    real beta_bar;
+    real gamma_bar;
+    real nu_bar;
+    real<lower=0> sigma_a;
+    real<lower=0> sigma_n;
+    real<lower=0> sigma_b;
+    real<lower=0> etasq_b;
+    real<lower=0> rhosq_b;
+    real<lower=0> sigma_g;
+    real<lower=0> etasq_g;
+    real<lower=0> rhosq_g;
+}
+transformed parameters{
+    vector[L] alpha;
+    vector[L] beta;
+    vector[L] gamma;
+    vector[L] nu;
+    matrix[L, L] L_SIGMA_b;
+    matrix[L, L] L_SIGMA_g;
+
+    alpha = exp(zalpha * sigma_a + alpha_bar);
+    nu = exp(znu * sigma_n + nu_bar)+1;
+
+    L_SIGMA_b = cholesky_decompose(cov_GPL2(Dmat_b, etasq_b, rhosq_b, sigma_b));
+    beta = L_SIGMA_b * zbeta + beta_bar;
+
+    L_SIGMA_g = cholesky_decompose(cov_GPL2(Dmat_g, etasq_g, rhosq_g, sigma_g));
+    gamma = L_SIGMA_g * zgamma + gamma_bar;
+    gamma = exp(gamma);
+    
+    for (i in 1:L){
+       gamma[i] = sqrt(sqrt(2*gamma[i]) * tgamma(3/nu[i]) / tgamma(1/nu[i]));
+    }
+}
+model{
+    // matrix[L,N] p;
+
+    sigma_a ~ exponential( 1 );
+    sigma_n ~ exponential( 2 );
+    sigma_b ~ exponential( 1 );
+    sigma_g ~ exponential( 1 );
+    etasq_b ~ exponential( 1 );
+    etasq_g ~ exponential( 1 );
+    rhosq_b ~ exponential( 0.5 );
+    rhosq_g ~ exponential( 0.5 );
+    alpha_bar ~ normal( 0 , 1.3 );
+    beta_bar ~ std_normal();
+    gamma_bar ~ std_normal();
+    nu_bar ~ std_normal();
+    zalpha ~ std_normal();
+    zgamma ~ std_normal();
+    zbeta ~ std_normal();
+    znu ~ std_normal();
+
+    for ( i in 1:L ){
+        for( j in 1:N ){
+           Y[i, j] ~ binomial(1, exp(-alpha[i] - pow(gamma[i] * fabs(X1[j] - beta[i]), nu[i])) + minp);
+        }
+    }
+    // Y ~ binomial(1, to_vector(p));
+}
+generated quantities{
+    vector[L*N] log_lik;
+    int k;
+    
+    k = 1;
+    for ( i in 1:L ){
+        for (j in 1:N){
+           log_lik[k] = binomial_lpmf(Y[i, j] | 1, exp(-alpha[i] - pow(gamma[i] * fabs(X1[j] - beta[i]), nu[i])) + minp);
+           k = k + 1;
+        }
+    }
+}
+"
 
 # Binomial 1d
 base.model.traits.1d <- "
