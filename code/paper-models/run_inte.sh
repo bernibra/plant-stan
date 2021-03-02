feb1e2916ea574dbcf7f61bf362cd42910484dab
bernibra <bernat.bramon@gmail.com>
2021-03-02 13:33:48 +0100

    Final fixes for first run of fat-tail model

--- code/paper-models/run_inte.sh
+++ code/paper-models/run_inte.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bsub -R "rusage[mem=4548]" -W 200:00 -n 15 "R --vanilla --slave < 1d-categorical.R"
+bsub -R "rusage[mem=4548]" -W 120:00 -n 15 "R --vanilla --slave < 1d-baseline.R"
