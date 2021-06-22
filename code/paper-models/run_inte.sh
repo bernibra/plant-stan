c0bbf1c0782229dd1dbba0cf44057969c021e727
bernibra <bernat.bramon@gmail.com>
2021-06-22 14:59:46 +0200

    run_inte

--- code/paper-models/run_inte.sh
+++ code/paper-models/run_inte.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bsub -R "rusage[mem=3500]" -W 10:00 -n 35 "R --vanilla --slave < 1d-baseline.R"
+bsub -R "rusage[mem=3500]" -W 10:00 -n 35 "R --vanilla --slave < 2d-baseline.R"
